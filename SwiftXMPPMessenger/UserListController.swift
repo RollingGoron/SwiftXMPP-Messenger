9c2d65d94176a26438554c18bf0373192fbfd643
your-github-username <pgosling@asu.edu>
2016-05-19 22:12:06 -0700

    Added Controllers for User List and Chat

---
+++ SwiftXMPPMessenger/UserListController.swift
@@ -0,0 +1,35 @@
+//
+//  UserListController.swift
+//  SwiftXMPPMessenger
+//
+//  Created by Peter Gosling on 5/19/16.
+//  Copyright © 2016 SunfireSoft. All rights reserved.
+//
+
+import UIKit
+
+class UserListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
